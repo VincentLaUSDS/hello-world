adc2eefe173d43692f77c53e8a6bab3976034f75
Vincent La <vincent.la@cloverhealth.com>
2017-05-18 22:49:11 -0700

    Adding new stuff

--- postgres/metadata_tips_and_tricks.sql
+++ postgres/metadata_tips_and_tricks.sql
@@ -7,27 +7,54 @@ comments, indices, constraints
 select
   *
 from information_schema.table_constraints
-where table_name = <table_name>
+where table_schema = <table_schema>
+  and table_name = <table_name>
+;
+
+--Selecting constraints along with check clauses
+select
+  tc.*,
+  cc.check_clause
+from information_schema.table_constraints as tc
+  left join information_schema.check_constraints as cc
+    on tc.constraint_schema = cc.constraint_schema
+      and tc.constraint_name = cc.constraint_name
+;
+
+--Selecting Keys (Both unique and primary)
+select
+  kcu.table_schema,
+  kcu.table_name,
+  kcu.constraint_name,
+  tc.constraint_type,
+  kcu.column_name,
+  kcu.ordinal_position,
+  kcu.position_in_unique_constraint
+from information_schema.key_column_usage as kcu
+left join information_schema.table_constraints as tc
+  on kcu.table_schema = tc.table_schema
+    and kcu.table_name = tc.table_name
+    and kcu.constraint_name = tc.constraint_name
 ;
 
 --Selecting Indices
 select
-    t.oid as oid,
-    t.relname as table_name,
-    i.relname as index_name,
-    a.attname as column_name
-from
-    pg_class t,
-    pg_class i,
-    pg_index ix,
-    pg_attribute a
-where
-    t.oid = ix.indrelid
-    and i.oid = ix.indexrelid
-    and a.attrelid = t.oid
-    and a.attnum = ANY(ix.indkey)
-    and t.relkind = 'r'
-    and t.relname = <table_name>
+  t.oid as oid,
+  ns.nspname as table_schema,
+  t.relname as table_name,
+  i.relname as index_name,
+  a.attname as column_name
+from pg_class as t
+  join pg_index as ix
+    on t.oid = ix.indrelid
+  join pg_class as i
+    on i.oid = ix.indexrelid
+  join pg_attribute as a
+    on a.attrelid = t.oid
+      and a.attnum = any(ix.indkey)
+  join pg_namespace as ns
+    on t.relnamespace = ns.oid
+  and t.relkind = 'r'
 order by
     t.relname,
     i.relname
